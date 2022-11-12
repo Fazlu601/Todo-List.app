0969eb69a588f53aa0a320b3cbfb59e4601946ff
Fazlu601 <fazrlu9575@gmail.com>
2022-11-12 10:22:55 +0700

    fix storage bug

--- src/redux/reducer/operation.js
+++ src/redux/reducer/operation.js
@@ -7,6 +7,7 @@ import { ADD_TODO,
 
 import {TODO_KEY, storageExist} from "../../storage/localStorage";
 
+storageExist([]);
 const storage = localStorage.getItem(TODO_KEY);
 
 const initialState = JSON.parse(storage);
